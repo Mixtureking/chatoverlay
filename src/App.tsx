9f0e719ee2f8e7642d5fe2d032bb59bd62231063
ArwassUwU <33115032+Mixtureking@users.noreply.github.com>
2026-06-05 11:50:34 +0700

    feat(deps): import useMemo hook in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import { createPortal } from "react-dom";
 import { ChatMessage, OverlaySettings, FilterKeyword, StreamStatus } from "./types";
 import OverlayWidget from "./components/OverlayWidget";
